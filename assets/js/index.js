1ef3352ba011500bec547edc73f6915c883bf8d4
pradnyashil012 <56130908+pradnyashil012@users.noreply.github.com>
2021-05-31 19:15:36 +0530

    Update use successful

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,28 @@
 $('#add_user').submit(function(event){
     alert("Data inserted successfully!");
 });
+
+$('#update_user').submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value'];
+    });
+
+    console.log(data);
+
+    var request = {
+        'url' : `http://localhost:3000/api/users/${data.id}`,
+        'method' : 'PUT',
+        'data' : data    
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data updated successfully!");
+    });
+
+
+})
